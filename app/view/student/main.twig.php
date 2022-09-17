cef8580e29640972059ffbf5f6246e312ddb3fc1
Vitor Aquino <vitor.aquino123@outlook.com>
2022-09-16 21:26:13 -0300

    1° tela de login finalizada

--- app/view/student/main.twig.php
+++ app/view/student/main.twig.php
@@ -5,24 +5,27 @@
 {% block body %}
 <div class="w-screen h-screen bg-[url({{BASE}}/imgs/background.png)] bg-contain bg-no-repeat bg-center bg-lightpurple relative overflow-x-hidden flex flex-row items-center justify-center">
 
-    <div class="flex flex-col items-center justify-center mr-2">
-        
-        <button class="bg-purple h-32 w-32 rounded-lg mb-16">
-            <i class="ph-user text-5xl text-white mt-1 ml-2"></i>
-            <span class="pl-2"></span>
-        </button>
 
-        <h1 class="text-orange font-bold mb-4">
-            Escolha seu Avatar!
-        </h1>
+    <div class="flex space-y-2 grid-cols rounded-lg items-center justify-center bg-white px-16 py-8 shadow-lg">
 
-        <button class="bg-pink rounded-md px-14 h-10 text-white flex items-center group mt-4">
-            <span class="pl-2">Aluno</span>
-        </button>
+        <div class="flex flex-col items-center justify-center mr-16">
+            
+            <button class="bg-purple h-32 w-32 rounded-lg mb-16">
+                <i class="ph-user text-5xl text-white mt-1 ml-2"></i>
+                <span class="pl-2"></span>
+            </button>
 
-    </div>
+            <h1 class="text-orange font-bold mb-4">
+                Escolha seu Avatar!
+            </h1>
 
-    <div class="flex space-y-2 flex-col rounded-lg items-center justify-center bg-white px-32 py-8 shadow-lg">
+            <button class="bg-pink rounded-md px-14 h-10 text-white flex items-center group mt-4">
+                <span class="pl-2">Aluno</span>
+            </button>
+
+        </div>
+
+<div class="flex flex-col items-center">
 
         <!-- Nome -->
         <div class="mt-2 font-bold flex flex-col">
@@ -78,6 +81,9 @@
                 <span class="pl-2">Cadastro</span>
             </button>
     </div>
+
+</div>
+        
     
 </div>
 {% endblock %}
