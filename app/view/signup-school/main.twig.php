ea89e4c71591ac182b18333b5728d17965a0075f
Marina <marinanogueira934@gmail.com>
2022-10-07 21:17:00 -0300

    responsividade da página signup-school

--- app/view/signup-school/main.twig.php
+++ app/view/signup-school/main.twig.php
@@ -5,22 +5,24 @@
 {% block body %}
     <div class="w-screen h-screen bg-[url({{BASE}}/imgs/background.png)] bg-contain bg-no-repeat bg-center bg-darkerPurple relative overflow-x-hidden flex flex-row items-center justify-center">
 
-        <div class="flex space-y-2 grid-cols rounded-md items-center justify-center bg-white px-8 py-12 shadow-lg">
+        <div class="text-lg flex flex-col md:flex-row lg:flex-row space-y-2 grid-cols absolute inset-x-0 bottom-0 md:static lg:static md:inset-0 lg:inset-0 rounded-md items-center bg-white h-[40rem] md:h-[20rem] lg:h-[20rem]  px-8 py-12 shadow-lg">
 
-            <div class="flex flex-col items-center justify-center mr-32">
+            <div class="flex flex-col items-center justify-center mr-0 md:mr-32 lg:mr-32">
 
-                <button class="bg-darkpurple hover:bg-purple cursor-default h-20 w-20 rounded-full mb-2">
+              <div class="mt-[-20rem] md:mt-0 lg:mt-0 items-center justify-center fixed md:static lg:static">
+                <button class="bg-darkpurple hover:bg-purple cursor-default h-20 w-20 rounded-full mb-2 ml-0 md:ml-10 lg:ml-10">
                     <i class="ph-user text-5xl text-white mt-1 ml-2"></i>
                     <span class="pl-2"></span>
                 </button>
 
-                <h1 class="text-orange font-bold">
+                <h1 class="text-orange font-bold ml-4 md:ml-14 lg:ml-14">
                     Perfil
                 </h1>
 
-                <p class="px-12 h-10 text-darkpurple font-bold flex items-center group mt-8">
+                <p class="px-0 md:px-12 lg:px-12 h-10 text-darkpurple font-bold flex items-center group mt-2">
                     <span class="pl-2">Escola</span>
                 </p>
+              </div>
                 
             </div>
 
@@ -50,8 +52,8 @@
                     >
                 </div>
             
-                <button class="bg-orange rounded-md w-36 h-9 text-white flex items-center group mt-8">
-                    <span class="pl-2 ml-8">Cadastrar</span>
+                <button class="bg-orange rounded-md w-36 h-9 text-white flex items-center justify-center group mt-8">
+                    <span class="pl-2">Cadastrar</span>
                 </button>
             </div>
 
