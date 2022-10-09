d7c6f8ea96965a71d3492508001f8fccd5f4571b
Victor Silva <victor.silvaPC@hotmail.com>
2022-10-09 19:25:09 -0300

    fixed sinup-student page

--- app/view/signup-student/main.twig.php
+++ app/view/signup-student/main.twig.php
@@ -5,52 +5,65 @@
 {% block body %}
     <div class="w-screen h-screen bg-[url({{BASE}}/imgs/background.png)] bg-contain bg-no-repeat bg-center bg-lightpurple relative overflow-x-hidden flex flex-row items-center justify-center">
 
-        <div class="flex space-y-2 grid-cols rounded-md items-center justify-center bg-white px-8 py-4 shadow-lg">
+        <main class="flex flex-wrap space-y-2 gap-4 rounded-md items-start justify-center bg-white px-8 py-8 shadow-lg w-full max-w-[600px] min-h-[350px]">
 
-        <form class="flex flex-col items-center" method="post" action="{{BASE}}@signup-student">
+            <div class="flex w-full h-full min-h-[350px] items-center justify-around flex-1 flex-col">
+                <div class="p-8 bg-grey rounded-md drop-shadow-sm h-min cursor-pointer">
+                    <img class="w-full h-full max-w-[130px] max-h-[130px]" src="{{BASE}}/svgs/anonymous_user.svg" alt="User without avatar"/>
+                </div>
 
-            <div class="mt-2 font-bold flex flex-col">
-                <label for="name">Nome</label>
-                <input class="bg-grey rounded-md h-8 w-full"
-                id="name"
-                type="text"
-                name="name"
-                >
+                <h2 class="font-nerko text-orange font-semibold">Escolha seu avatar!</h2>
+
+                <p class="bg-pink-500 text-white px-4 py-2 cursor-default rounded-md drop-shadow-sm min-w-[130px] text-center">Aluno</p>
+
             </div>
 
-            <div class="mt-2 font-bold flex flex-col">
-                <label for="email">E-mail</label>
-                <input class="bg-grey rounded-md h-8 w-full"
-                id="email"
-                type="text"
-                name="email"
-                >
+            <div id="divider" class="hidden 2sm:block min-h-[350px] w-[2px] bg-grey text-grey drop-shadow-sm rounded-md mx-2">
             </div>
 
-            <div class="mt-2 font-bold flex flex-col w-full">
-                <label for="birthday">Data de Nascimento</label>
-                <input class="bg-grey rounded-md h-8 w-full"
-                id="birthday"
-                type="date"
-                name="birthday"
-                >
-            </div>
+            <form class="h-full w-full flex-1 flex flex-col items-center px-6 " method="post" action="{{BASE}}@signup-student">
 
-            <div class="mt-2 font-bold flex flex-col">
-                <label for="password">Senha</label>
-                <input class="bg-grey rounded-md h-8 w-full"
-                id="password"
-                type="password"
-                name="password"
-                >
-            </div>
+                <div class="font-bold flex flex-col w-full">
+                    <label for="name">Nome</label>
+                    <input class="bg-grey rounded-md h-10 w-full"
+                    id="name"
+                    type="text"
+                    name="name"
+                    >
+                </div>
 
-            <button class="bg-orange rounded-md px-12 h-10 text-white flex items-center group mt-8" type="submit">
-                <span class="pl-2">Cadastrar</span>
-            </button>
-         </form>
+                <div class="mt-2 font-bold flex flex-col w-full">
+                    <label for="email">E-mail</label>
+                    <input class="bg-grey rounded-md h-10 w-full"
+                    id="email"
+                    type="text"
+                    name="email"
+                    >
+                </div>
 
-</div>
+                <div class="mt-2 font-bold flex flex-col w-full">
+                    <label for="birthday">Data de Nascimento</label>
+                    <input class="bg-grey rounded-md h-10 w-full"
+                    id="birthday"
+                    type="date"
+                    name="birthday"
+                    >
+                </div>
+
+                <div class="mt-2 font-bold flex flex-col w-full">
+                    <label for="password">Senha</label>
+                    <input class="bg-grey rounded-md h-10 w-full"
+                    id="password"
+                    type="password"
+                    name="password"
+                    >
+                </div>
+
+                <button class="bg-orange rounded-md px-12 h-10 text-white flex items-center group mt-8" type="submit">
+                    <span class="pl-2">Cadastrar</span>
+                </button>
+            </form>
+        </main>
         
     </div>
 {% endblock %}
