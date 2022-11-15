23626a8f709102756ce2e107a5fcfe19e108f14d
Vitor Aquino <vitor.aquino123@outlook.com>
2022-11-15 12:53:25 -0300

    barra de progresso adicionada

--- app/view/dashboard/main.twig.php
+++ app/view/dashboard/main.twig.php
@@ -7,51 +7,53 @@
 {% endblock %}
 
 {% block main %}
-<div class="">
+<div class="ml-12">
   <sl-tab-group>
 
-    <sl-tab slot="nav" panel="perfil">
-      Perfil
+    <sl-tab slot="nav"panel="perfil">
+      <h1 class="text-3xl font-nerko">Perfil</h1>
     </sl-tab>
 
     <sl-tab slot="nav" panel="sala de aula">
-      Sala de aula
+      <h1 class="text-3xl font-nerko" >Sala de aula</h1>
     </sl-tab>
 
     <sl-tab slot="nav" panel="configurações">
-      Configurações
+      <h1 class="text-3xl font-nerko" >Configurações</h1>
     </sl-tab>
 
     <sl-tab slot="nav" panel="ajuda">
-      Ajuda
+      <h1 class="text-3xl font-nerko" >Ajuda</h1>
     </sl-tab>
         
-        <!-- Perfil -->
+        <!-- sessão Perfil -->
         <sl-tab-panel name="perfil">
           <h1 class="text-4xl text-lightpurple mt-4 font-nerko">
             Perfil
           </h1>
-          <div class="">
+          <div>
 
-            <div class="flex flex-col">
-              <label class="mt-4 text-xl font-bold" for="nome">Nome</label>
-              <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
+            <div class="grid grid-cols-2 grid-row-2">
+              <div class="flex flex-col">
+                <label class="mt-4 text-xl font-bold" for="nome">Nome</label>
+                <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
+                  type="text"
+                  id="nome">
+              </div>          
+              
+              <div class="flex flex-col mt-4">
+                <label class="text-xl font-bold" for="nome">Username</label>
+                <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
                 type="text"
-                id="nome">
-            </div>          
-            
-            <div class="flex flex-col">
-              <label class="text-xl font-bold" for="nome">Username</label>
-              <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
-              type="text"
-              id="username">
-            </div>
-            
-            <div class="flex flex-col">
-              <label class="text-xl font-bold" for="nome">Conta</label>
-              <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
-              type="text"
-              id="conta">
+                id="username">
+              </div>
+              
+              <div class="flex flex-col">
+                <label class="text-xl font-bold" for="nome">Conta</label>
+                <input class="bg-grey rounded-lg h-[2rem] w-[14rem]"
+                type="text"
+                id="conta">
+              </div>              
             </div>
           
             <div class="flex flex-col">
@@ -65,7 +67,7 @@
 
           <h1 class="text-4xl text-lightpurple mt-4 font-nerko">Conta</h1>
 
-          <div class="">
+          <div class="grid grid-cols-2 grid-row-2">
 
             <div>
               <h2 class="text-xl mt-2 font-bold">
@@ -115,7 +117,7 @@
           </div>
         </sl-tab-panel>
 
-        <!-- sala de aula -->
+        <!-- sessão sala de aula -->
         <sl-tab-panel name="sala de aula">
           <div class="flex gap-[6rem] justify-around">
             <h1 class="text-4xl text-orange font-nerko">
@@ -133,7 +135,7 @@
             <p class="text-lg font-bold">Professor Xavier</p>
           </div>
 
-          <div class="flex gap-[25rem] justify-center mt-[5rem]">
+          <div class="flex gap-[15rem] justify-start mt-[5rem]">
               <h2 class="text-4xl text-orange font-nerko">
                 Alunos
               </h2>
@@ -143,15 +145,26 @@
               </h2>
           </div>
 
-          <div class="flex justify-around mt-6">
-            <div class="grid gap-[360px] grid-cols-2 grid-row-2">
-              <div>
-                <p class="text-lg font-bold mt-2">Aleksander Morozova</p>
-                <p class="text-lg font-bold mt-2">Giovana Nocera</p>
-                <p class="text-lg font-bold mt-2">Thiago Muratian</p>               
+          <div class="flex justify-start mt-6">
+            <div class="grid gap-[240px] grid-cols-2 grid-row-2">
+              <div class="space-y-4">
+                <p class="text-lg font-bold mt-2">
+                  Aleksander Morozova
+                  <sl-progress-bar value="67"></sl-progress-bar>
+                </p>
+
+                <p class="text-lg font-bold mt-2">
+                  Giovana Nocera
+                  <sl-progress-bar value="59"></sl-progress-bar>
+                </p>
+
+                <p class="text-lg font-bold mt-2">
+                  Thiago Muratian
+                  <sl-progress-bar value="48"></sl-progress-bar>
+                </p>               
               </div>
 
-              <div>
+              <div class="space-y-8 mt-8">
                 <p class="text-lg font-bold mt-2">67%</p>
                 <p class="text-lg font-bold mt-2">59%</p>
                 <p class="text-lg font-bold mt-2">48%</p>
@@ -159,7 +172,6 @@
 
             </div>
           </div>
-
         </sl-tab-panel>
 
         <sl-tab-panel name="configurações">
