684570a5f776db5a7ecbf17d90ac1d3dc67a14be
vitor aquino <vitor.aquino123@outlook.com>
2022-11-25 15:26:39 -0300

    school dashboard fixed

--- app/view/dashboard-school/main.twig.php
+++ app/view/dashboard-school/main.twig.php
@@ -7,12 +7,12 @@
 {% endblock %}
 
 {% block main %}
-    <div class="md:ml-[17.5rem]">
+    <div class="md:ml-[17.5rem] flex justify-center">
         <sl-tab-group id="tab-group">
 
             <!-- TABS -->
-            <sl-tab slot="nav" panel="perfil" class="tab">
-            <h1 class="text-3xl font-nerko">Perfil</h1>
+            <sl-tab slot="nav" panel="painel" class="tab">
+            <h1 class="text-3xl font-nerko">Painel</h1>
             </sl-tab>
 
             <sl-tab slot="nav" panel="turmas" class="tab">
@@ -26,12 +26,142 @@
             <sl-tab slot="nav" panel="ajuda" class="tab">
             <h1 class="text-3xl font-nerko" >Ajuda</h1>
             </sl-tab>
-                
-            <!-- PERFIL TAB CONTENT -->
-            <sl-tab-panel name="perfil">
 
+            <!-- PERFIL TAB CONTENT -->
+            <sl-tab-panel name="painel">
+                <div class="mt-8 flex justify-between rounded-sm bg-white drop-shadow-lg w-[45rem] h-[3rem] items-center">
+                    <h1 class="ml-4 text-4xl text-orange font-nerko">Geral</h1>
+                    <p class="mr-4 font-bold text-xl">Turmas
+                        <button>
+                            <span class="ml-12 ph-caret-double-down"></span>
+                        </button>
+                    </p>
+                </div>
+
+                <h1 class="flex justify-center mt-8 text-4xl text-orange font-nerko">Média Geral da Escola</h1>
+            </sl-tab-panel>
+
+            <!-- CLASS TAB CONTENT -->
+            <sl-tab-panel name="turmas">
+                <h1 class="text-4xl font-nerko text-orange">Turma</h1>
+
+                <div class="flex justify-start mt-4">
+                    <div class="flex gap-[25rem] justify-around rounded-sm bg-white drop-shadow-lg w-[45rem] h-[3rem] items-center">
+                        <button class="bg-lightpurple rounded-md w-[8rem] h-[2rem] drop-shadow-lg">
+                            <span class="font-nerko font-bold text-2xl">Nova +</span>
+                        </button>
+                        <input class="rounded-sm h-[2rem] w-[11rem]"
+                        type="text"
+                        >
+                    </div>
+                </div>
+
+                <div class="flex gap-[8.5rem] justify-start mt-[5rem]">
+                    <h2 class="text-4xl text-orange font-nerko">
+                        Nome
+                    </h2>
+
+                    <h2 class="text-4xl text-orange font-nerko">
+                        Código
+                    </h2>
+                    
+                    <h2 class="text-4xl text-orange font-nerko">
+                        Professor
+                    </h2>
+                </div>
+
+                <div class="flex justify-start mt-6">
+                    <div class="grid gap-[9rem] grid-cols-4 grid-row-4">
+                        <div class="space-y-6">
+                            <p class="text-lg font-bold">
+                            2A-Tarde
+                            </p>
+
+                            <p class="text-lg font-bold">
+                            4A-Manhã
+                            </p>
+
+                            <p class="text-lg font-bold">
+                            4B-Manhã
+                            </p>
+
+                            <p class="text-lg font-bold">
+                            4C-Tarde
+                            </p>
+                            
+                            <p class="text-lg font-bold">
+                            5A-Manhã
+                            </p>
+                            
+                            <p class="text-lg font-bold">
+                            5B-Manhã
+                            </p>
+                        </div>
+
+                        <div class="space-y-[1.8rem]">
+                            <p>JEOIS234</p>
+                            <p>FHSIE835</p>
+                            <p>FHSIE835</p>
+                            <p>URIDS934</p>
+                            <p>HDEJN904</p>
+                            <p>AHFIE367</p>
+                        </div>
+
+                        <div class="space-y-[1.8rem]">
+                            <p>Gilmar</p>
+                            <p>Gilmar</p>
+                            <p>Antônio</p>
+                            <p>Antônio</p>
+                            <p>Renata</p>
+                            <p>Renata</p>
+                        </div>
+
+                        <div class="flex flex-col space-y-[1.7rem] items-center">
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <span class="text-xl ph-pencil"></span>
+                                    <span class="text-xl ph-trash"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </sl-tab-panel>
+
+            <sl-tab-panel name="configurações">
+                
                 <div>
-                    <!-- Perfil -->
+                    <!-- Painel -->
                     <h1 class="text-4xl text-lightpurple my-4 font-nerko">Perfil</h1>                    
                     <div class="flex flex-col 2sm:grid-cols-2 gap-8">
                         <div class="flex flex-col gap-2">
@@ -77,126 +207,6 @@
                     </div>
 
                 </div>
-            </sl-tab-panel>
-
-            <!-- CLASS TAB CONTENT -->
-            <sl-tab-panel name="turmas">
-                <h1 class="text-4xl font-nerko text-orange">Turma</h1>
-
-                <div class="flex justify-start mt-4">
-                    <div class="flex gap-[35rem] justify-around rounded-sm bg-white drop-shadow-lg w-[900px] h-[50px] items-center">
-                        <button class="bg-lightpurple rounded-md w-[115px] h-[30px] drop-shadow-lg">
-                            <span class="font-nerko font-bold text-2xl">Nova +</span>
-                        </button>
-                        <input class="rounded-sm h-[1.8rem] w-[10.5rem]"
-                        type="text"
-                        >                    
-                    </div>
-                </div>
-
-                <div class="flex gap-[10rem] justify-start mt-[5rem]">
-                    <h2 class="text-4xl text-orange font-nerko">
-                        Nome
-                    </h2>
-
-                    <h2 class="text-4xl text-orange font-nerko">
-                        Código
-                    </h2>
-                    
-                    <h2 class="text-4xl text-orange font-nerko">
-                        Professor
-                    </h2>
-                </div>
-
-                <div class="flex justify-start mt-6">
-                    <div class="grid gap-[10rem] grid-cols-4 grid-row-4">
-                        <div class="space-y-8">
-                            <p class="text-lg font-bold">
-                            2A-Tarde
-                            </p>
-
-                            <p class="text-lg font-bold">
-                            4A-Manhã
-                            </p>
-
-                            <p class="text-lg font-bold">
-                            4B-Manhã
-                            </p>
-
-                            <p class="text-lg font-bold">
-                            4C-Tarde
-                            </p>
-                            
-                            <p class="text-lg font-bold">
-                            5A-Manhã
-                            </p>
-                            
-                            <p class="text-lg font-bold">
-                            5B-Manhã
-                            </p>
-                        </div>
-
-                        <div class="space-y-[2.3rem]">
-                            <p>JEOIS234</p>
-                            <p>FHSIE835</p>
-                            <p>FHSIE835</p>
-                            <p>URIDS934</p>
-                            <p>HDEJN904</p>
-                            <p>AHFIE367</p>
-                        </div>
-
-                        <div class="space-y-[2.3rem]">
-                            <p>Gilmar</p>
-                            <p>Gilmar</p>
-                            <p>Antônio</p>
-                            <p>Antônio</p>
-                            <p>Renata</p>
-                            <p>Renata</p>
-                        </div>
-
-                        <div class="flex flex-col space-y-9 items-center">
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <span class="text-xl ph-pencil"></span>
-                                    <span class="text-xl ph-trash"></span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </sl-tab-panel>
-
-            <sl-tab-panel name="configurações">
             </sl-tab-panel>
 
             <sl-tab-panel name="ajuda">
